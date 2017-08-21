a6900961cc612b18ba16d4879beb76e1253e9163
Jacob Trueb <trueb2@illinois.edu>
2017-08-21 14:31:11 -0500

    Added Material-UI and JSS

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,9 +1,46 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import injectSheet, { JssProvider, SheetsRegistry } from 'react-jss'
+import { create } from 'jss';
+import preset from 'jss-preset-default';
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+import createPalette from 'material-ui/styles/palette';
+import createGenerateClassName from 'material-ui/styles/createGenerateClassName';
+import { green, red } from 'material-ui/colors';
 
-export default class App extends Component {
+const styles = {
+  main: {
+    fontFamily: '"Roboto", sans-serif'
+  }
+};
+
+class App extends Component {
   render() {
+    // Create a sheetsRegistry instance.
+    const sheetsRegistry = new SheetsRegistry();
+
+    // Create a theme instance.
+    const theme = createMuiTheme({
+      palette: createPalette({
+        primary: green,
+        accent: red,
+        type: 'light',
+      }),
+    });
+
+    // Configure JSS
+    const jss = create(preset());
+    jss.options.createGenerateClassName = createGenerateClassName;
+
     return (
-      <h1>Triangle</h1>
+      <div className={this.props.classes.main}>
+        <JssProvider registry={sheetsRegistry} jss={jss}>
+          <MuiThemeProvider theme={theme} sheetsManager={new Map()}>
+            <h1>Triangle</h1>
+          </MuiThemeProvider>
+        </JssProvider>
+      </div>
     );
   }
 }
+
+export default injectSheet(styles)(App);
